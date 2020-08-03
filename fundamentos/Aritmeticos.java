b492a5e92be618e7f6d3528b62499fe17ecb0637
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-03 14:29:43 -0300

    Create Aritmeticos.java

---
+++ fundamentos/Aritmeticos.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+public class Aritmeticos {
+	public static void main(String[] args) {
+		System.out.println(2 + 3);
+		
+		var x = 12.65;
+		double y = 1.5;
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println(x / y);
+		
+		Integer a = 5;
+		int b = 3;
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println((double) a / b);
+		
+		System.out.println(a % b);
+	}
+}
