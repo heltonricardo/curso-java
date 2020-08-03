78f22b875c7a61f15e1c662fe80a18804675a2d7
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-03 17:10:00 -0300

    Update DesafioAritmeticos.java

--- fundamentos/DesafioAritmeticos.java
+++ fundamentos/DesafioAritmeticos.java
@@ -8,8 +8,6 @@ public class DesafioAritmeticos {
 		double b = Math.pow((1 - 5) * (2 - 7) / 2, 2);
 		double c = Math.pow(a - b, 3) / Math.pow(10, 3);
 		
-		System.out.println((int) a);
-		System.out.println((int) b);
-		System.out.println((int) c);
+		System.out.println("O resultado da expressão é: " + (int) c);
 	}
 }
