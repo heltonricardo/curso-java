ed5f2007211ae10f4726fd24a9cd90650b0829cf
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-04 13:20:32 -0300

    Create Relacionais.java

---
+++ fundamentos/operadores/Relacionais.java
@@ -0,0 +1,15 @@
+package fundamentos.operadores;
+
+public class Relacionais {
+
+	public static void main(String[] args) {
+		
+		System.out.println(3 == 7);
+		System.out.println(3 > 7);
+		System.out.println(3 < 7);
+		System.out.println(3 <= 3);
+		System.out.println(4 >= 3);
+		System.out.println(4 != 3);
+		
+	}
+}
