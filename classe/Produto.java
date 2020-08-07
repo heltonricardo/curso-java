af01d3a73621959e9cfecd2b344febe0288a65db
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-07 18:32:06 -0300

    Construtor

--- classe/Produto.java
+++ classe/Produto.java
@@ -6,6 +6,16 @@ public class Produto {
 	double preco;
 	double desconto;
 	
+	Produto() {
+		
+	}
+	
+	Produto(String nome, double preco, double desconto) {
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
+	
 	double precoComDesconto() {
 		return this.preco * (1 - this.desconto);
 	}
