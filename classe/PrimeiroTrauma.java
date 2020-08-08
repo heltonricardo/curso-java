6d9a74ca46a97169412043a01375f3e7377b2266
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-08 12:24:13 -0300

    Update PrimeiroTrauma.java

--- classe/PrimeiroTrauma.java
+++ classe/PrimeiroTrauma.java
@@ -3,11 +3,13 @@ package classe;
 public class PrimeiroTrauma {
 
 	int a = 3;
+	static int c = 4;
 	
 	public static void main(String[] args) {
 		
 		PrimeiroTrauma b = new PrimeiroTrauma();
-		
 		System.out.println(b.a);
+		
+		System.out.println(c);
 	}
 }
