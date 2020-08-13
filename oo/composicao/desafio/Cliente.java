9955bca2d76362ba2aa4731843907b607ab06229
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-13 17:47:52 -0300

    Create Cliente.java

---
+++ oo/composicao/desafio/Cliente.java
@@ -0,0 +1,14 @@
+package oo.composicao.desafio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+
+	String nome;
+	List<Compra> lista = new ArrayList<Compra>();
+	
+	double obterValorTotal() {
+		return 0;
+	}
+}
