face247eef6acac5f4404e80de7b902ee60ee56e
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-13 11:56:25 -0300

    Update Curso.java

--- oo/composicao/Curso.java
+++ oo/composicao/Curso.java
@@ -5,8 +5,19 @@ import java.util.List;
 
 public class Curso {
 
-	String nome;
-	List<Aluno> alunos = new ArrayList<>();
+	final String nome;
+	final List<Aluno> alunos = new ArrayList<>();
 	
+	Curso(String nome) {
+		this.nome = nome;
+	}
 	
+	void adicionarAluno(Aluno aluno) {
+		this.alunos.add(aluno);
+		aluno.cursos.add(this);
+	}
+	
+	public String toString() {
+		return "Curso: " + this.nome;
+	}
 }
