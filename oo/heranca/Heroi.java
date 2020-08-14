f9bdaa2989fe558ce27788704152c5875a145af4
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-14 13:14:04 -0300

    Update Heroi.java

--- oo/heranca/Heroi.java
+++ oo/heranca/Heroi.java
@@ -2,5 +2,10 @@ package oo.heranca;
 
 public class Heroi extends Jogador {
 
-	
+	boolean atacar(Jogador oponente) {
+		boolean atq1 = super.atacar(oponente);
+		boolean atq2 = super.atacar(oponente);
+		boolean atq3 = super.atacar(oponente);
+		return atq1 || atq2 || atq3;
+	}
 }
