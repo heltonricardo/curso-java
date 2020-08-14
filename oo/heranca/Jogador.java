139dc6b43c384c26a23a322a73993211cd146fba
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-14 13:03:37 -0300

    Update Jogador.java

--- oo/heranca/Jogador.java
+++ oo/heranca/Jogador.java
@@ -3,21 +3,27 @@ package oo.heranca;
 public class Jogador {
 
 	int x, y;
+	int vida = 100;
+	
+	boolean atacar(Jogador oponente) {
+		
+		int deltaX = Math.abs(this.x - oponente.x);
+		int deltaY = Math.abs(this.y - oponente.y);
+		
+		if (deltaX + deltaY == 1) {
+			oponente.vida -= 10;
+			return true;
+		}
+		
+		return false;
+	}
 	
 	boolean andar(Direcao direcao) {
 		switch (direcao) {
-			case NORTE:
-				--this.y;
-				break;
-			case SUL:
-				++this.y;
-				break;
-			case LESTE:
-				++this.x;
-				break;
-			case OESTE:
-				--this.x;
-				break;
+			case NORTE: --this.y; break;
+			case SUL:   ++this.y; break;
+			case LESTE: ++this.x; break;
+			case OESTE: --this.x; break;
 		}
 		return true;
 	}
