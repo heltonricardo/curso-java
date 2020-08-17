152860387d55757bf652b7a8699da7ff545d66d4
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-17 11:21:03 -0300

    Update CarroTeste.java

--- oo/heranca/teste/CarroTeste.java
+++ oo/heranca/teste/CarroTeste.java
@@ -11,36 +11,36 @@ public class CarroTeste {
 		Carro fusca = new Fusca(100);
 		Carro laFerrari = new LaFerrari();
 		
-		System.out.println("Fusca:     " + fusca.velocidadeAtual + " km/h.");
-		System.out.println("LaFerrari: " + laFerrari.velocidadeAtual + " km/h.");
+		System.out.println("Fusca:     " + fusca + " km/h.");
+		System.out.println("LaFerrari: " + laFerrari + " km/h.");
 		System.out.println();
 		
 		fusca.acelerar();
 		laFerrari.acelerar();
 	
-		System.out.println("Fusca:     " + fusca.velocidadeAtual + " km/h.");
-		System.out.println("LaFerrari: " + laFerrari.velocidadeAtual + " km/h.");
+		System.out.println("Fusca:     " + fusca + " km/h.");
+		System.out.println("LaFerrari: " + laFerrari + " km/h.");
 		System.out.println();
 		
 		fusca.acelerar();
 		laFerrari.acelerar();
 		
-		System.out.println("Fusca:     " + fusca.velocidadeAtual + " km/h.");
-		System.out.println("LaFerrari: " + laFerrari.velocidadeAtual + " km/h.");
+		System.out.println("Fusca:     " + fusca + " km/h.");
+		System.out.println("LaFerrari: " + laFerrari + " km/h.");
 		System.out.println();
 		
 		fusca.frear();
 		laFerrari.frear();
 		
-		System.out.println("Fusca:     " + fusca.velocidadeAtual + " km/h.");
-		System.out.println("LaFerrari: " + laFerrari.velocidadeAtual + " km/h.");
+		System.out.println("Fusca:     " + fusca + " km/h.");
+		System.out.println("LaFerrari: " + laFerrari + " km/h.");
 		System.out.println();
 
 		fusca.frear();
 		laFerrari.frear();
 		
-		System.out.println("Fusca:     " + fusca.velocidadeAtual + " km/h.");
-		System.out.println("LaFerrari: " + laFerrari.velocidadeAtual + " km/h.");
+		System.out.println("Fusca:     " + fusca + " km/h.");
+		System.out.println("LaFerrari: " + laFerrari + " km/h.");
 		System.out.println();
 	}
 }
