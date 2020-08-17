3d7e9b360e9bc89664af423061e7d94ccec8500b
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-17 11:41:05 -0300

    Update Heroi.java

--- oo/heranca/Heroi.java
+++ oo/heranca/Heroi.java
@@ -6,10 +6,17 @@ public class Heroi extends Jogador {
 		super(x, y);
 	}
 
+	@Override
 	public boolean atacar(Jogador oponente) {
 		boolean atq1 = super.atacar(oponente);
 		boolean atq2 = super.atacar(oponente);
 		boolean atq3 = super.atacar(oponente);
 		return atq1 || atq2 || atq3;
+	}
+	
+	// Quando há sobreescrita, deve-se manter ou aumentar a visibilidade.
+	@Override
+	public boolean andar(Direcao direcao) {
+		return super.andar(direcao);
 	}
 }
