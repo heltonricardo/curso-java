767d544dc9b28863a50b40679b52eb60a70d084e
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-17 11:41:17 -0300

    Update Monstro.java

--- oo/heranca/Monstro.java
+++ oo/heranca/Monstro.java
@@ -9,4 +9,9 @@ public class Monstro extends Jogador {
 	public Monstro(int x, int y) {
 		super(x, y);
 	}
+	
+	@Override
+	public boolean atacar(Jogador oponente) {
+		return super.atacar(oponente);
+	}
 }
