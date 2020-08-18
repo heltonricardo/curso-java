5a890f9db1f78c6cdb8b7d10435203ec116c1323
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-18 11:23:14 -0300

    Update Calculo.java

--- lambdas/Calculo.java
+++ lambdas/Calculo.java
@@ -1,5 +1,9 @@
 package lambdas;
 
+// A anotação FunctionalInterface serve para garantir que uma \
+// interface, que será usada com lambda, terá apenas uma função
+
+@FunctionalInterface
 public interface Calculo {
 
 	double executar(double a, double b);
