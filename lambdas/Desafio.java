357eb8b52839c4430039f97b5117f10c4f4a3bc3
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-20 20:11:36 -0300

    Update Desafio.java

--- lambdas/Desafio.java
+++ lambdas/Desafio.java
@@ -11,13 +11,18 @@ public class Desafio {
 		Function<Produto, Double> precoReal = p -> p.preco * (1 - p.desconto);
 		UnaryOperator<Double> imposto = i -> i >= 2500 ? i * 1.085 : i;
 		UnaryOperator<Double> frete = f -> f >= 3000 ? f + 100 : f + 50;
-		Function<Double, String> arredondar = a -> String.format("%.2f", a);
-		UnaryOperator<String> formatar = f -> "R$ " + f;
+		UnaryOperator<Double> arredondar = a -> Double.parseDouble(String.format("%.2f", a).replace(',',  '.'));
+		Function<Double, String> formatar = f -> "R$ " + f.toString().replace('.', ',');
 		
 		Produto prod = new Produto("iPad", 3235.89, 0.13);
 		
-		String r = precoReal.andThen(imposto).andThen(frete).andThen(arredondar).andThen(formatar).apply(prod);
+		String r = precoReal
+				.andThen(imposto)
+				.andThen(frete)
+				.andThen(arredondar)
+				.andThen(formatar)
+				.apply(prod);
 		
-		System.out.println(r);
+		System.out.println("Valor final: " + r);
 	}
 }
