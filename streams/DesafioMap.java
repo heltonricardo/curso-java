305d2cec577153e90f4683751c7c12ecf8ca3a79
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-08-24 15:06:24 -0300

    Create DesafioMap.java

---
+++ streams/DesafioMap.java
@@ -0,0 +1,21 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class DesafioMap {
+
+	public static void main(String[] args) {
+		
+		Consumer<Integer> print = System.out::println; 
+		
+		List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		
+		nums.stream().
+		map(DesafioUtil.binString).
+		map(DesafioUtil.inverter).
+		map(DesafioUtil.strBinary).
+		forEach(print);
+	}
+}
