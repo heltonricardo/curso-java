a5b3feea7f9db6cbf8f1620a31f20d8ccbf25a6a
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-08 15:29:21 -0300

    Create AniverssarioSurpresa.java

---
+++ padroes/observer/AniverssarioSurpresa.java
@@ -0,0 +1,19 @@
+package padroes.observer;
+
+public class AniverssarioSurpresa {
+
+	public static void main(String[] args) {
+		
+		Namorada namorada = new Namorada();
+		Porteiro porteiro = new Porteiro();
+		
+		porteiro.registrarObservador(namorada);
+		porteiro.registrarObservador(e -> {
+			System.out.println("Usando lambda.");
+			System.out.println("Ocorreu em: " + e.getMomento());
+		});
+			
+		
+		porteiro.monitorar();
+	}
+}
