262c0c1313273327ba507f99d48e6d7fab1efa44
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-10-13 10:10:31 -0300

    Update FabricaConexao.java

--- jdbc/FabricaConexao.java
+++ jdbc/FabricaConexao.java
@@ -1,25 +1,33 @@
 package jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
 	public static Connection getConexao() {
-		final String url1 = "jdbc:mysql://localhost/CURSO_JAVA?"; 
-		final String url2 = "verifyServerCertificate=false&"; 
-		final String url3 = "useSSL=true&"; 
-		final String url4 = "useTimezone=true&serverTimezone=UTC"; 
-		
-		final String url = url1 + url2 + url3 + url4;				
-		final String usuario = "root";
-		final String senha = "12345";
 		
 		try {
+			Properties prop = getProperties();
+			final String url = prop.getProperty("banco.url");				
+			final String usuario = prop.getProperty("banco.usuario");
+			final String senha = prop.getProperty("banco.senha");
+		
 			return DriverManager.getConnection(url, usuario, senha);
-		} catch (SQLException e) {
+		} catch (SQLException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+	
+	// Não é aconselhável deixar o arquivo que contém as informações de conexão \
+	// junto com o projeto que será compilado. Você deve externalizá-lo!
+	private static Properties getProperties() throws IOException {
+		Properties prop = new Properties();
+		String caminho = "/conexao.properties";
+		prop.load(FabricaConexao.class.getResourceAsStream(caminho));
+		return prop;
 	}
 }
